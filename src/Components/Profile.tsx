07e89880188f5c1949155f814fcce2db3fdf043b
DmitryGK <gulyakevichdmitry@gmail.com>
2022-12-14 02:15:00 +0300

    Components_1

---
+++ src/Components/Profile.tsx
@@ -0,0 +1,23 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+        <div>
+          img
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My posts
+          <div>
+            New post
+            <div>post 1</div>
+            <div>post 2</div>
+          </div>
+        </div>
+        Main content
+      </div>
+    )
+}
+
+export default Profile
