5482bbaa97709b37b0980173f60744802130e68b
DmitryGK <gulyakevichdmitry@gmail.com>
2022-12-18 22:30:10 +0300

    add images

--- src/Components/Profile/MyPosts/Post/Post.tsx
+++ src/Components/Profile/MyPosts/Post/Post.tsx
@@ -1,5 +1,5 @@
 import i from './Post.module.css'
-
+import ava from './images/01f1a5fb119543d29c04c714710e9c46.jpg'
 
 
 type PostPropsType = {
@@ -9,7 +9,7 @@ type PostPropsType = {
 export const Post = (props: PostPropsType) => {
     return (
         <div className={i.item}>
-            <img src='./images/01f1a5fb119543d29c04c714710e9c46.jpg'/>
+            <img src={ava}/>
             {props.message}
             <div>
                 like
--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,11 +1,12 @@
 import { MyPosts } from './MyPosts/MyPosts'
 import c from './Profile.module.css'
+import bg from './../Profile/MyPosts/Post/images/photo-1535136104956-115a2cd67fc4.avif'
 
 const Profile = () => {
     return (
         <div className={c.content}>
             <div>
-                img
+                <img src={bg}/>
             </div>
             <div>
                 ava + description
